67a1a0d6e2ed30d523fee130443cd280544807fd
José Victor Zeller <josevictorzg@gmail.com>
2019-05-05 20:17:56 -0300

    refactor: change stochastic validor method

--- src/Neuron/Neuron_Validator/Stochastic.py
+++ src/Neuron/Neuron_Validator/Stochastic.py
@@ -3,7 +3,8 @@ Created on 3 de mai de 2019
 
 @author: zeller
 '''
-from Validator.Validator import Validator
+from Neuron.Neuron_Validator.Validator import Validator
+
 
 class Stochastic(Validator):
     '''
@@ -18,5 +19,5 @@ class Stochastic(Validator):
         '''
    
     def validate(self, sum_input_values, theta):
-        return 1 if sum_input_values > theta else 0
+        return 1 if sum_input_values + theta >= 0 else 0
         
